b15bb2e52614c0001d7f6be3b8f328a211a1bf7b
Taki <laszlo@letmewebyou.com>
2021-01-09 22:20:54 +0100

    Next: Adding Prop Type Validation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Aux from "../../hoc/Auxilliary";
 import classes from "./Layout.module.css";
 
+
 const layout = (props) => (
   <Aux>
-  <div>Valami, 2,3</div>
-    <main className={classes.Component}>Teszt</main>
+  <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
     </Aux>
 )
 
