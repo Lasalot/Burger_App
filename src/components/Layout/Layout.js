1d50dfbad55fc6487b2012ece0a0008cdc243531
Taki <laszlo@letmewebyou.com>
2021-01-09 22:00:56 +0100

    Next : Creating a Layout Component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from "../../hoc/Auxilliary";
+import classes from "./Layout.module.css";
+
+const layout = (props) => (
+  <Aux>
+  <div>Valami, 2,3</div>
+    <main className={classes.Component}>Teszt</main>
+    </Aux>
+)
+
+export default layout;
